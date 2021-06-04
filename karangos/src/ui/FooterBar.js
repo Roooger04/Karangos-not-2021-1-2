e45e45cb243571e9bdf991ea6c6cffd0b61e8c02
Roooger04 <rogerrogerionascimento01@gmail.com>
2021-06-04 20:42:24 +0000

    pausa

---
+++ karangos/src/ui/FooterBar.js
@@ -0,0 +1,9 @@
+import Toolbar from '@material-ui/core/Toolbar'
+
+export default function FooterBar(){
+    return(
+        <Toolbar>
+            Desenvolvido por <a href="mailto:rogerrogerio97@gmail.com">Roger Rogerio</a>
+        </Toolbar>
+    )
+}
