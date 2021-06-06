863ab5466f0bc69b27852fd8e41033e38476a7b8
Roooger04 <rogerrogerionascimento01@gmail.com>
2021-06-06 16:03:56 +0000

    fui almoçar dia 06

--- karangos/src/routed/ClienteForm.js
+++ karangos/src/routed/ClienteForm.js
@@ -2,6 +2,12 @@ import { useState, useEffect } from 'react'
 import TextField from '@material-ui/core/TextField'
 import MenuItem from '@material-ui/core/MenuItem'
 import { makeStyles } from '@material-ui/core/styles'
+import InputMask from 'react-input-mask'
+import { Button, Toolbar } from '@material-ui/core'
+import axios from 'axios'
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { useHistory } from 'react-router-dom'
 
 const useStyles = makeStyles(() => ({
     form: {
@@ -14,9 +20,36 @@ const useStyles = makeStyles(() => ({
             minWidth: '200px',
             maxWidth: '500px',
             marginBottom: '24px'
-        }
+        },
+        toolbar: { 
+            marginTop: '36px',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-around'
+        },
     },
 }))
+
+/* Classes de caractere de entrada para a mascara do campo CPF,RG,numero do imóvel 
+ 1) Três primeiras posições: qualquer letra, de A a Z (maiscula ou miniscula) ~> [A-Za-z]
+ 2) Posições numericas (1°, a 3° e a 4° depois do traço) ~> [0-9]
+ 3) 2° posição apos o traço: pode receber digitos ou letras de A a J (maisculas ou minuscalas) ~> [0-9A-Ja-j]
+*/
+
+// Representando as classes de caracteres da mascara como um objeto
+const formatChars = {
+    'A': '[A-Za-z]',
+    '0': '[0-9]',
+    '#': '[0-9A-Ja-j]'
+}
+
+// curiosidade: Mascara para CNPJ: '00.000.000/0000-00'
+// Finalmente, a mascara de entrada do campo CPF
+// Mascara para CPF: '000.000.000-00'
+const cpfMask = '000.000.000-00'
+const rgMask = '00.000.000-0/AAA-AA'
+const telefoneMask = '(00) 00000-0000'
+
 
 export default function ClienteForm(){
     const classes = useStyles()
@@ -38,11 +71,28 @@ export default function ClienteForm(){
 
     const [currentId, setCurrentId] = useState()
 
+    const [snackState, setSnackState] = useState({
+        open: false,
+        severity: 'success',
+        message: 'Cliente salvo com sucesso'
+    })
+
+    const [btnSendState, setBtnSendState] = useState({
+        disabled: false,
+        label: 'Enviar'
+    })
+
+    const history = useHistory()
+
     function handleInputChange(event, property) {
 
         // Se houver id no event.target, ele será o nome da propriedade
         // se não, usaremos o valor do segundo parâmetro
         if(event.target.id) property = event.target.id
+
+        if(property === 'rg'){ // era para deichar a expedição do RG maiscula ****CORRIGIR*****
+            setClientes({...clientes, [property]: event.target.value.toUpperCase()})
+        }
 
         // Quando o nome de uma propriedade de um objeto aparece entre [],
         // isso se chama "propriedade calculada". O nome da propriedade vai
@@ -52,19 +102,71 @@ export default function ClienteForm(){
         
     }
 
+    async function saveData(){
+        try {
+            // Desabilitar botão
+            setBtnSendState({disabled: true, label: 'Enviando...'})
+
+         await axios.post('https://api.faustocintra.com.br/clientes', clientes)
+         setSnackState({
+            open: true,
+            severity: 'success',
+            message: 'Cliente salvo com sucesso'
+        })  
+        }
+        catch(error) {
+            setSnackState({
+                open: true,
+                severity: 'error',
+                message: 'ERRO: ' + error.message
+            }) 
+        }
+        // Reabilitar o botão de enviar
+        setBtnSendState({disabled: false, label: 'Enviar'})
+    }
+
+    function handleSubmit(event) {
+
+        event.preventDefault() // Envia o recarregamento da página
+        
+        saveData()
+
+    }
+
+    function Alert(props) {
+        return <MuiAlert elevation={6} variant="filled" {...props} />;
+    }
+
+    function handleSnackClose(event, reason) {
+        // Evita que a snackbar seja fechada clicando-se fora
+        if(reason === 'clickaway') return 
+        setSnackState({...snackState, open: false}) // Fecha a snackbar
+    }
+
     return (
         <>
+
+            <Snackbar open={snackState.open} autoHideDuration={6000} onClose={handleSnackClose}>
+            <Alert onClose={handleSnackClose} severity={snackState.severity}>
+                {snackState.message}
+            </Alert>
+            </Snackbar>
+
             <h1>Cadastrar Novo Cliente</h1>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={handleSubmit}>
 
                 <TextField id="nome" label="Nome" variant="filled" value={clientes.nome} 
                 onChange={handleInputChange} fullWidth/>
 
-                <TextField id="cpf" label="CPF" variant="filled" value={clientes.cpf} 
-                onChange={handleInputChange} fullWidth/>
+                <InputMask formatChars={formatChars} mask={cpfMask} id="cpf" value={clientes.cpf} 
+                onChange={event => handleInputChange(event, 'cpf')}>
+                    {() => <TextField  label="CPF" variant="filled" fullWidth/>}
+                </InputMask>
 
-                <TextField id="rg" label="RG" variant="filled" value={clientes.rg} 
-                onChange={handleInputChange} fullWidth/>
+                <InputMask formatChars={formatChars} mask={rgMask} id="rg" value={clientes.rg} 
+                onChange={event => handleInputChange(event, 'rg')}>
+                    {() => <TextField  label="RG" variant="filled" fullWidth/>}
+                </InputMask>                
 
                 <TextField id="logradouro" label="Logradouro" variant="filled" value={clientes.logradouro} 
                 onChange={handleInputChange} fullWidth/>
@@ -111,6 +213,19 @@ export default function ClienteForm(){
                     <MenuItem value="TO">Tocantins</MenuItem>
                     <MenuItem value="DF">Distrito Federal</MenuItem>
                 </TextField>
+
+                <InputMask formatChars={formatChars} mask={telefoneMask} id="telefone" value={clientes.telefone} 
+                onChange={event => handleInputChange(event, 'telefone')}>
+                    {() => <TextField  label="Telefone" variant="filled" fullWidth/>}
+                </InputMask>
+
+                <TextField id="email" label="E-mail" variant="filled" value={clientes.email} 
+                onChange={handleInputChange} fullWidth/>
+
+                <Toolbar className={classes.toolbar}>
+                    <Button variant="contained" color="secondary" type="submit">Enviar</Button>
+                    <Button variant="contained">Voltar</Button>
+                </Toolbar>
 
                 <div>{JSON.stringify(clientes)}<br />currentId: {currentId}</div>
             </form>
--- karangos/src/routed/KarangosForm.js
+++ karangos/src/routed/KarangosForm.js
@@ -5,6 +5,13 @@ import { makeStyles } from '@material-ui/core/styles'
 import FormControl from '@material-ui/core/FormControl'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Checkbox from '@material-ui/core/Checkbox'
+import InputMask from 'react-input-mask'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import { Button, Toolbar } from '@material-ui/core'
+import axios from 'axios'
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { useHistory } from 'react-router-dom'
 
 const useStyles = makeStyles(() => ({
     form: {
@@ -19,7 +26,35 @@ const useStyles = makeStyles(() => ({
             marginBottom: '24px'
         }
     },
+    toolbar: { 
+        marginTop: '36px',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-around'
+    },
+    checkbox: {
+        alignItems: 'center',
+    },
 }))
+
+/* Classes de caractere de entrada para a mascara do campo placa 
+ 1) Três primeiras posições: qualquer letra, de A a Z ()maiscula ou miniscula ~> [A-Za-z]
+ 2) Posições numericas (1°, a 3° e a 4° depois do traço) ~> [0-9]
+ 3) 2° posição apos o traço: pode receber digitos ou letras de A a J (maisculas ou minuscalas) ~> [0-9A-Ja-j]
+*/
+
+// Representando as classes de caracteres da mascara como um objeto
+const formatChars = {
+    'A': '[A-Za-z]',
+    '0': '[0-9]',
+    '#': '[0-9A-Ja-j]'
+}
+
+// Finalmente, a mascara de entrada do campo placa
+const placaMask = 'AAA-0#00'
+
+// Mascara para CPF: '000.000.000-00'
+// Mascara para CNPJ: '00.000.000/0000-00'
 
 export default function KarangosForm(){
     const classes = useStyles()
@@ -38,6 +73,19 @@ export default function KarangosForm(){
     const [currentId, setCurrentId] = useState()
     const [importadoChecked, setImportadoChecked] = useState()
 
+    const [snackState, setSnackState] = useState({
+        open: false,
+        severity: 'success',
+        message: 'Karango salvo com sucesso'
+    })
+
+    const [btnSendState, setBtnSendState] = useState({
+        disabled: false,
+        label: 'Enviar'
+    })
+
+    const history = useHistory()
+
     function handleInputChange(event, property) {
 
         // Se houver id no event.target, ele será o nome da propriedade
@@ -48,6 +96,9 @@ export default function KarangosForm(){
             const newState = ! importadoChecked
             setKarangos({...karangos, importado: (newState ? '1': '0')})
             setImportadoChecked(newState)
+        }
+        else if(property === 'placa'){
+            setKarangos({...karangos, [property]: event.target.value.toUpperCase()})
         }
         else{
             // Quando o nome de uma propriedade de um objeto aparece entre [],
@@ -64,10 +115,59 @@ export default function KarangosForm(){
         return result
     }
 
+    async function saveData(){
+        try {
+            // Desabilitar botão
+            setBtnSendState({disabled: true, label: 'Enviando...'})
+
+         await axios.post('https://api.faustocintra.com.br/karangos', karangos)
+         
+         setSnackState({
+             open: true,
+             severity: 'success',
+             message: 'Karango salvo com sucesso!'
+         }) 
+        }
+        catch(error) {
+            setSnackState({
+                open: true,
+                severity: 'error',
+                message: 'ERRO: ' + error.message
+            }) 
+        }
+        // Reabilitar o botão de enviar
+        setBtnSendState({disabled: false, label: 'Enviar'})
+    }
+
+    function Alert(props) {
+        return <MuiAlert elevation={6} variant="filled" {...props} />;
+    }
+
+    function handleSnackClose(event, reason) {
+        // Evita que a snackbar seja fechada clicando-se fora
+        if(reason === 'clickaway') return 
+        setSnackState({...snackState, open: false}) // Fecha a snackbar
+    }
+
+    function handleSubmit(event) {
+
+        event.preventDefault() // Envia o recarregamento da página
+        
+        saveData()
+
+    }
+
     return (
         <>
+
+            <Snackbar open={snackState.open} autoHideDuration={6000} onClose={handleSnackClose}>
+            <Alert onClose={handleSnackClose} severity={snackState.severity}>
+                {snackState.message}
+            </Alert>
+            </Snackbar>
+
             <h1>Cadastrar Novo Karango</h1>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={handleSubmit}>
 
                 <TextField id="marca" label="Marca" variant="filled" value={karangos.marca}
                 onChange={handleInputChange} fullWidth/>
@@ -97,13 +197,42 @@ export default function KarangosForm(){
                     {years().map(year => <MenuItem value ={year}>{year}</MenuItem>)}                    
                 </TextField>
 
-                <FormControl fullWidth>
-                <FormControlLabel
-                    control={<Checkbox checked={importadoChecked} onChange={handleInputChange} 
-                    id="importado" />}
-                    label="Importado?"
+                <InputMask formatChars={formatChars} mask={placaMask} id="placa" 
+                onChange={event => handleInputChange(event, 'placa')} value={karangos.placa}>
+                    {() => <TextField label="Placa" variant="filled" fullWidth/>}
+                </InputMask>
+
+                <TextField 
+                id="preco" 
+                label="Preço" 
+                variant="filled" 
+                value={karangos.preco}
+                onChange={handleInputChange} 
+                fullWidth 
+                type="number"
+                onFocus={event => event.target.select()}
+                InputProps={{
+                    startAdornment: <InputAdornment position="start">R$</InputAdornment>,
+                }}
                 />
+
+                <FormControl className={classes.checkbox} fullWidth>
+                    <FormControlLabel
+                        control={<Checkbox checked={importadoChecked} onChange={handleInputChange} 
+                        id="importado" />}
+                        label="Importado?"
+                    />
                 </FormControl>
+
+                <Toolbar className={classes.toolbar}>
+                    <Button 
+                    variant="contained" 
+                    color="secondary" 
+                    type="submit">
+                        {btnSendState.label}
+                    </Button>
+                    <Button variant="contained">Voltar</Button>
+                </Toolbar>
 
                 <div>{JSON.stringify(karangos)}<br />currentId: {currentId}</div>
             </form>
